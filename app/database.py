06428f6ac31c63d2af120e8132d8850c813679a8
Anshul-Bhau <anshulbhau1@gmail.com>
2026-06-26 22:23:27 +0530

    MySQL connection added

---
+++ app/database.py
@@ -0,0 +1,34 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, 
+    AsyncSession,
+    async_sessionmaker,
+)
+
+from sqlalchemy.orm import DeclerativeBase
+from app.config import settings
+
+# Create async engine
+# pool_pre_pings = True : test connections before use (handles MySQL timeouts)
+engine = create_async_engine(
+    settings.MYSQL_URL,
+    echo=settings.APP_ENV == "development", # Log SQL in dev only
+    pool_pre_ping = True,
+    pool_size = 10,
+    max_overflow = 20,
+)
+
+# Session
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False, 
+)
+
+# Base class
+class Base(DeclerativeBase):
+    pass
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
